482560e5ee043f9685f6a771ae1e10c7d0eedbd4
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-01-31 15:02:28 +0100

    feat(security): ajout de la configuration Spring Security avec authentification JWT et configuration CORS

---
+++ back/src/main/java/com/openclassrooms/mddapi/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.openclassrooms.mddapi.security.filter;
+
+import com.openclassrooms.mddapi.feature.JsonWebToken;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.Collections;
+
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JsonWebToken jsonWebToken;
+
+    public JwtAuthenticationFilter(JsonWebToken jsonWebToken) {
+        super();
+        this.jsonWebToken = jsonWebToken;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException, java.io.IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            if (jsonWebToken.isTokenValid(token)) {
+                Claims claims = jsonWebToken.getClaimsFromToken(token);
+                String username = claims.getSubject(); // "sub"
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
