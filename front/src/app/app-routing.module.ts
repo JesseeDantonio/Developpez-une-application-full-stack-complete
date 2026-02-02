152224f906ddef61f16caa06f609d46d9d205dd5
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-02-02 16:55:25 +0100

    feat(): Mise en place du header, avancement de la page register

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -8,12 +8,17 @@ import { RegisterComponent } from './register/register.component';
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 export const routes: Routes = [
-    {
+  {
     path: '',
-    component: LayoutComponent, // 1. Le parent est le Layout
+    component: HomeComponent,
+    pathMatch: 'full' // <--- C'est la clé ! Cela signifie "Uniquement si l'URL est exactement vide"
+  },
+
+  // 2. Le Layout (Parent des pages d'auth)
+  {
+    path: '', // Ce path vide sert de "préfixe" pour les enfants
+    component: LayoutComponent,
     children: [
-      // 2. Les enfants s'afficheront A L'INTERIEUR du <router-outlet> du Layout
-      { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
     ]
