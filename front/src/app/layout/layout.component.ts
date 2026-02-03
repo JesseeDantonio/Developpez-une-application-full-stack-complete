84e9ab6e50c70b13e19fdab4c527a24018056d45
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-02-03 14:57:36 +0100

    style(layout): appliquer le design final

--- front/src/app/layout/layout.component.ts
+++ front/src/app/layout/layout.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from "@angular/router";
-
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AsyncPipe, Location } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
 @Component({
-    selector: 'app-layout',
-    templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss'],
-    standalone: true,
-    imports: [RouterModule]
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  standalone: true,
+  imports: [RouterModule, AsyncPipe],
 })
 export class LayoutComponent implements OnInit {
+  private breakpointObserver = inject(BreakpointObserver);
 
-  constructor(private router: Router) { }
+  isDesktop$ = this.breakpointObserver
+    .observe([Breakpoints.Tablet, Breakpoints.Large, Breakpoints.XLarge])
+    .pipe(map((result) => result.matches));
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private location: Location,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {}
 
   public goToHome() {
-    this.router.navigate(['/'])
+    this.router.navigate(['/']);
   }
 
+  public goBack() {
+    this.location.back();
+  }
 }
