726dff4dd65f4c008139ebba6b7b6d8c9912647d
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-02-06 13:40:14 +0100

    feat(config, auth): ajout de l'intercepteur des requetes

---
+++ front/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  // Si on a un token, on clone la requête pour y ajouter le header
+  if (token) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest);
+  }
+
+  // Sinon on laisse passer la requête telle quelle (ex: login/register)
+  return next(req);
+};
