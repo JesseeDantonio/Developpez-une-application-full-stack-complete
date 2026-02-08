5f85148aa03eabb323d9ccbaee59b5a5c6e5cfef
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-02-08 16:13:33 +0100

    feat(form): Mise en place de la logique des formulaires

--- front/src/app/features/auth/login/login.component.ts
+++ front/src/app/features/auth/login/login.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginRequest } from 'src/app/core/models/auth.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: true
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [ReactiveFormsModule],
+  standalone: true
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
+  ngOnInit(): void { }
+
+  onSubmit() {
+    console.log("Form submitted with values:", this.loginForm.value);
+    if (this.loginForm.invalid) {
+      return; 
+    }
+    const credentials: LoginRequest = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        console.log('Login successful', response);
+        localStorage.setItem('token', response.token);
+      },
+      error: (err) => console.error('Erreur login', err)
+    });
+  }
 }
