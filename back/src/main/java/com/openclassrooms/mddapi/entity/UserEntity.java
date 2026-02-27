f6a7290df75226637cf2f2ea8876a9dea867cd25
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-02-27 13:41:10 +0100

    refactor(user): remplacement @Data avec @Getter et @Setter dans UserEntity

--- back/src/main/java/com/openclassrooms/mddapi/entity/UserEntity.java
+++ back/src/main/java/com/openclassrooms/mddapi/entity/UserEntity.java
@@ -5,9 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @Entity
+@Getter
+@Setter
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
