3461ec547578dc04b8543291ee8d515c1f683af3
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-02-27 13:30:36 +0100

    feat(article): ajout de la liste themeIds dans CreateArticleDTO

--- back/src/main/java/com/openclassrooms/mddapi/dto/in/CreateArticleDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/in/CreateArticleDTO.java
@@ -3,6 +3,8 @@ package com.openclassrooms.mddapi.dto.in;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CreateArticleDTO {
     private String title;
@@ -12,4 +14,6 @@ public class CreateArticleDTO {
     private String createdAt;
     @JsonProperty("updated_at")
     private String updatedAt;
+
+    private List<Integer> themeIds;
 }
