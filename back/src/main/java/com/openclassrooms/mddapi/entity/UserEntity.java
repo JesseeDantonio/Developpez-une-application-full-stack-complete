596d9a2f3986b1e781f04d8d1531db9018c8eba9
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-02-27 15:21:52 +0100

    feat(user): ajout de la relation ManyToMany entre UserEntity et ThemeEntity pour les abonnements

--- back/src/main/java/com/openclassrooms/mddapi/entity/UserEntity.java
+++ back/src/main/java/com/openclassrooms/mddapi/entity/UserEntity.java
@@ -1,12 +1,11 @@
 package com.openclassrooms.mddapi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Entity
 @Getter
@@ -24,4 +23,12 @@ public class UserEntity {
     private String createdAt;
 
     private String updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "theme_subscription",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "theme_id")
+    )
+    private Set<ThemeEntity> subscribedThemes;
 }
