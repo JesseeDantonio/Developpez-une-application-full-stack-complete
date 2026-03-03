93b0b9449069593c375576529b09f18bafecc6cd
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-03 15:34:51 +0100

    feat(article): ajout du service ArticleService pour la gestion des articles

---
+++ front/src/app/features/article/services/article.service.ts
@@ -0,0 +1,16 @@
+// features/article/services/article.service.ts
+import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { ArticleDto } from './../../../core/dto/in/articleDto'; // Ou ton DTO
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root' // 'root' le rend dispo partout, c'est OK pour un singleton
+})
+export class ArticleService extends ApiService<ArticleDto> {
+  constructor(http: HttpClient) {
+    super(http, 'articles'); // Juste le endpoint 'articles'
+  }
+  
+  // Méthodes spécifiques aux articles UNIQUEMENT ici
+}
