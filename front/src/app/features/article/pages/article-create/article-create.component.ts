2c3494811df6716274b3e8a805204bab49185127
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2026-03-04 11:57:18 +0100

    feat(article): implémentation de la logique réactive du formulaire pour article creation avec validation

--- front/src/app/features/article/pages/article-create/article-create.component.ts
+++ front/src/app/features/article/pages/article-create/article-create.component.ts
@@ -1,20 +1,53 @@
 import { Component } from '@angular/core';
-import { AsyncPipe, Location } from '@angular/common';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ArticleService } from '../../services/article.service';
+import { CreateArticleDto } from 'src/app/core/dto/out/CreateArticleDto';
 
 @Component({
   selector: 'app-create-article',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './article-create.component.html',
   styleUrl: './article-create.component.scss',
 })
 export class CreateArticleComponent {
 
+  articleCreateForm!: FormGroup;
 
-constructor(private location: Location) {}
-
+  constructor(private fb: FormBuilder, private location: Location, private articleService: ArticleService) {
+    this.articleCreateForm = this.fb.group({
+      theme: ['', Validators.required],
+      title: ['', Validators.required],
+      content: ['', Validators.required]
+    });
+  }
 
 
   public goBack() {
     this.location.back();
   }
+
+  onSubmit() {
+    // console.log("Form submitted with values:", this.articleCreateForm.value);
+    if (this.articleCreateForm.invalid) {
+      return;
+    }
+
+    const articleData : CreateArticleDto = {
+      title: this.articleCreateForm.value.title,
+      content: this.articleCreateForm.value.content,
+      themeIds: this.articleCreateForm.value.theme.split(',').map((id: string) => parseInt(id.trim())) // Convertit la chaîne de thèmes en tableau d'entiers
+    };
+
+    this.articleService.create(articleData).subscribe({
+      next: (response) => {
+        console.log("Article created successfully:", response);
+        this.goBack();
+      },
+      error: (error) => {
+        console.error("Error creating article:", error);
+      }
+    });
+  }
+
 }
