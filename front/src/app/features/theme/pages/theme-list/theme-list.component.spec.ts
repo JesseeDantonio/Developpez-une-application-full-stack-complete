e040b89e977c872f9d21a64d27136cc5448b24d5
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-05 09:58:22 +0100

    refactor(theme): changement, organisation de la structure
    	modified:   front/src/app/app-routing.module.ts

--- front/src/app/features/theme/pages/theme-list/theme-list.component.spec.ts
+++ front/src/app/features/theme/pages/theme-list/theme-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ThemeComponent } from './theme.component';
+import { ThemeComponent } from './theme-list.component';
 
 describe('ThemeComponent', () => {
   let component: ThemeComponent;
