ea6f2e14501a98ce3991a87d7b87049105031d84
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-05 09:25:11 +0100

    feat(routes): ajout des mécanismes d'authentification pour gérer l'accès des utilisateurs authentifiés et non authentifiés

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -10,6 +10,8 @@ import { ThemeComponent } from './features/theme/theme.component';
 import { CreateArticleComponent } from './features/article/pages/article-create/article-create.component';
 import { DetailArticleComponent } from './features/article/pages/article-detail/article-detail.component';
 import { ProfilComponent } from './features/profil/profil.component';
+import { unauthGuard } from './core/guards/unauth.guard';
+import { authGuard } from './core/guards/auth.guard';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -17,12 +19,14 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [unauthGuard],
     pathMatch: 'full' // <--- C'est la clé ! Cela signifie "Uniquement si l'URL est exactement vide"
   },
 
   // Le Layout (Parent des pages d'auth)
   {
     path: '', // Ce path vide sert de "préfixe" pour les enfants
+    canActivate: [unauthGuard], // Seules les personnes non authentifiées peuvent accéder à ces routes
     component: LayoutComponent,
     children: [
       { path: 'login', component: LoginComponent },
@@ -32,6 +36,7 @@ export const routes: Routes = [
 
   {
     path: '',
+    canActivate: [authGuard], // Seules les personnes authentifiées peuvent accéder à ces routes
     component: AuthLayoutComponent,
     children: [
       { path: 'article', component: ArticleComponent },
