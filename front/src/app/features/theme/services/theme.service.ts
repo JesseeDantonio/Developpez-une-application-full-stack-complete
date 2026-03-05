22c4655269999c4eee37c1be5e90bc3b47a5d5e3
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-05 10:20:43 +0100

    feat(theme): ajout du service ThemeService pour la gestion des thèmes

---
+++ front/src/app/features/theme/services/theme.service.ts
@@ -0,0 +1,16 @@
+// features/article/services/article.service.ts
+import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { HttpClient } from '@angular/common/http';
+import { ThemeDto } from 'src/app/core/dto/in/themeDto';
+
+@Injectable({
+  providedIn: 'root' // 'root' le rend dispo partout, c'est OK pour un singleton
+})
+export class ThemeService extends ApiService<ThemeDto> {
+  constructor(http: HttpClient) {
+    super(http, 'themes'); // Juste le endpoint 'themes'
+  }
+  
+  // Méthodes spécifiques aux themes UNIQUEMENT ici
+}
