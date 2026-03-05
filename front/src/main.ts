8701da544f40ba32a26f1760d352602f632b1e36
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-05 13:09:31 +0100

    feat(app): utilisation d'un fichier config pour séparer les responsabilités

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,17 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app-routing.module'
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-  ],
-}).catch((err) => console.error(err));
+// On démarre l'app en lui passant la config
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
