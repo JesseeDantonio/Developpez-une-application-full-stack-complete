c80999c0c3594c405bbfa100e734206a3d418026
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-05 10:49:54 +0100

    feat(article-create): implémentation de la logique pour la récupération des thèmes dans le formulaire

--- front/src/app/features/article/pages/article-create/article-create.component.ts
+++ front/src/app/features/article/pages/article-create/article-create.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ArticleService } from '../../services/article.service';
 import { CreateArticleDto } from 'src/app/core/dto/out/CreateArticleDto';
+import { ThemeService } from 'src/app/features/theme/services/theme.service';
+import { ThemeDto } from 'src/app/core/dto/in/themeDto';
 
 @Component({
   selector: 'app-create-article',
@@ -11,17 +18,32 @@ import { CreateArticleDto } from 'src/app/core/dto/out/CreateArticleDto';
   styleUrl: './article-create.component.scss',
 })
 export class CreateArticleComponent {
-
   articleCreateForm!: FormGroup;
-
-  constructor(private fb: FormBuilder, private location: Location, private articleService: ArticleService) {
+  themesList: ThemeDto[] = []; 
+  constructor(
+    private fb: FormBuilder,
+    private location: Location,
+    private articleService: ArticleService,
+    private themeService: ThemeService,
+  ) {
     this.articleCreateForm = this.fb.group({
       theme: ['', Validators.required],
       title: ['', Validators.required],
-      content: ['', Validators.required]
+      content: ['', Validators.required],
     });
   }
 
+  ngOnInit() {
+    this.themeService.getAll().subscribe({
+      next: (res) => {
+        console.log('Themes fetched successfully:', res);
+        this.themesList = res;
+      },
+      error: (error) => {
+        console.error('Error fetching themes:', error);
+      },
+    });
+  }
 
   public goBack() {
     this.location.back();
@@ -33,21 +55,22 @@ export class CreateArticleComponent {
       return;
     }
 
-    const articleData : CreateArticleDto = {
+    const articleData: CreateArticleDto = {
       title: this.articleCreateForm.value.title,
       content: this.articleCreateForm.value.content,
-      themeIds: this.articleCreateForm.value.theme.split(',').map((id: string) => parseInt(id.trim())) // Convertit la chaîne de thèmes en tableau d'entiers
+      themeIds: this.articleCreateForm.value.theme
+        .split(',')
+        .map((id: string) => parseInt(id.trim())), // Convertit la chaîne de thèmes en tableau d'entiers
     };
 
     this.articleService.create(articleData).subscribe({
       next: (response) => {
-        console.log("Article created successfully:", response);
+        console.log('Article created successfully:', response);
         this.goBack();
       },
       error: (error) => {
-        console.error("Error creating article:", error);
-      }
+        console.error('Error creating article:', error);
+      },
     });
   }
-
 }
