290b9e2665f851d04d43ed0bd73851c85f64a4fc
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-06 20:35:05 +0100

    feat(article-detail): récuperation de l'id utilisateur dans l'url + appel api article

--- front/src/app/features/article/pages/article-detail/article-detail.component.ts
+++ front/src/app/features/article/pages/article-detail/article-detail.component.ts
@@ -1,5 +1,7 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleDto } from 'src/app/core/dto/in/articleDto';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-detail-article',
@@ -7,10 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './article-detail.component.html',
   styleUrl: './article-detail.component.scss',
 })
-export class DetailArticleComponent {
-  constructor(private location: Location) {}
+export class DetailArticleComponent implements OnInit {
+
+  article: ArticleDto | null = null;
+
+  constructor(private location: Location, private articleService: ArticleService) { }
 
   public goBack() {
     this.location.back();
+  }
+
+  ngOnInit(): void {
+    const articleId = this.getArticleIdFromUrl();
+    this.articleService.getById(articleId).subscribe((article) => {
+      this.article = article;
+    });
+  }
+
+
+  public getArticleIdFromUrl(): number {
+    const url = this.location.path();
+    const segments = url.split('/');
+    const id = segments[segments.length - 1];
+    return parseInt(id, 10);
   }
 }
