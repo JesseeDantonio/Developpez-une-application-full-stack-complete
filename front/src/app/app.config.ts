5c293c201f027db220db43ffa2812d8bebee5b99
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-06 20:54:54 +0100

    refactor(app.config): Séparation des responsabilités

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,11 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-import { ApplicationConfig } from '@angular/core';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
+// Vérifie bien ces chemins d'importation selon ton projet
+import { routes } from './app-routing.module'; 
+import { authInterceptor } from './core/interceptors/auth.interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-
-    provideHttpClient(withInterceptors([authInterceptor])) 
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ]
 };
