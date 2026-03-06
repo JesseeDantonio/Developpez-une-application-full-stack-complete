e15eb65009621c85e0365669d2cbab042e5086ec
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-06 20:30:57 +0100

    feat(article-create): récupération de l'id utilisateur + navigation vers article-list après validation

--- front/src/app/features/article/pages/article-create/article-create.component.ts
+++ front/src/app/features/article/pages/article-create/article-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, Location } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
@@ -9,22 +9,28 @@ import {
 import { ArticleService } from '../../services/article.service';
 import { CreateArticleDto } from 'src/app/core/dto/out/CreateArticleDto';
 import { ThemeService } from 'src/app/features/theme/services/theme.service';
-import { ThemeDto } from 'src/app/core/dto/in/themeDto';
+import { ThemeDto } from 'src/app/core/dto/in/ThemeDto';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-create-article',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './article-create.component.html',
   styleUrl: './article-create.component.scss',
 })
-export class CreateArticleComponent {
-  articleCreateForm!: FormGroup;
-  themesList: ThemeDto[] = []; 
+export class CreateArticleComponent implements OnInit {
+  public articleCreateForm!: FormGroup;
+  public themes: Observable<ThemeDto[]> | null = null;
+
   constructor(
     private fb: FormBuilder,
     private location: Location,
+    private router: Router,
     private articleService: ArticleService,
     private themeService: ThemeService,
+    private authService: AuthService
   ) {
     this.articleCreateForm = this.fb.group({
       theme: ['', Validators.required],
@@ -34,15 +40,7 @@ export class CreateArticleComponent {
   }
 
   ngOnInit() {
-    this.themeService.getAll().subscribe({
-      next: (res) => {
-        console.log('Themes fetched successfully:', res);
-        this.themesList = res;
-      },
-      error: (error) => {
-        console.error('Error fetching themes:', error);
-      },
-    });
+     this.themes = this.themeService.getAll();
   }
 
   public goBack() {
@@ -50,12 +48,19 @@ export class CreateArticleComponent {
   }
 
   onSubmit() {
-    // console.log("Form submitted with values:", this.articleCreateForm.value);
     if (this.articleCreateForm.invalid) {
       return;
     }
 
+    const userId : string | null = this.authService.getUserIdFromToken();
+    console.log('User ID extracted from token:', userId);
+    if (userId === null) {
+      console.error('User ID not found in token. Please log in again.');
+      return;
+    }
+
     const articleData: CreateArticleDto = {
+      userId: userId,
       title: this.articleCreateForm.value.title,
       content: this.articleCreateForm.value.content,
       themeIds: this.articleCreateForm.value.theme
@@ -72,5 +77,7 @@ export class CreateArticleComponent {
         console.error('Error creating article:', error);
       },
     });
+
+    this.router.navigate(['/articles']);
   }
 }
