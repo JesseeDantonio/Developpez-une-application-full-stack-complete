8e43ed5394c8d8f1f2f572d4ef53258ef82e314b
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-06 20:23:40 +0100

    feat(article-list): ajout de la navigation vers la page detail + message si aucun article

--- front/src/app/features/article/pages/article-list/article-list.component.ts
+++ front/src/app/features/article/pages/article-list/article-list.component.ts
@@ -1,33 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { ArticleService } from '../../services/article.service';
 import { ArticleDto } from 'src/app/core/dto/in/articleDto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.scss',
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
 
-  public articlesList: ArticleDto[] = [];
-
+  public articles: ArticleDto[] = [];
   constructor(
     private router: Router,
-    private articleService: ArticleService,
+    private articleService: ArticleService
   ) {}
 
   ngOnInit() {
-    const monObservable$ = this.articleService.getAll();
-
-    monObservable$.subscribe((articles) => {
-      console.log(articles);
-      this.articlesList = articles;
+    this.articleService.getAll().subscribe((articles) => {
+      for (let article of articles) {
+        // Afficher le nom des utilisateurs au lieu de l'id
+      }
+      this.articles = articles;
     });
   }
 
   public goCreateArticle() {
     this.router.navigate(['/create-article']);
   }
+
+  public goArticleDetail(id: number) {
+    this.router.navigate(['/detail-article', id]);
+  }
+
+
 }
