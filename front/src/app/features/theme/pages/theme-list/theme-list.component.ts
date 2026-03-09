53ffd26b5d1b648e2a3aa8a7d86fd0ac7874f97c
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-09 14:41:43 +0100

    feat(theme-list): intégration de la logique pour les abonnements

--- front/src/app/features/theme/pages/theme-list/theme-list.component.ts
+++ front/src/app/features/theme/pages/theme-list/theme-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ThemeDto } from 'src/app/core/dto/in/ThemeDto';
 import { ThemeService } from '../../services/theme.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { Theme } from 'src/app/core/models/theme.interface';
 
 @Component({
   selector: 'app-theme',
@@ -10,14 +11,63 @@ import { ThemeService } from '../../services/theme.service';
 })
 export class ThemeComponent {
 
-  public themes: ThemeDto[] = [];
+  public themes: Theme[] = [];
 
-  constructor(private themeService: ThemeService) { }
+  constructor(
+    private themeService: ThemeService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit(): void {
+    const payload = this.tokenService.getPayloadFromToken();
+
+    if (payload == null) {
+      return;
+    }
+
     this.themeService.getAll().subscribe((themes) => {
-      this.themes = themes;
+      this.themeService.getSubscribedThemes(payload.sub).subscribe((subscribedThemes) => {
+        themes.forEach((theme) => {
+          this.themes.push(
+            {
+              id: theme.id.toString(),
+              name: theme.name.toString(),
+              description: theme.description.toString(),
+              isSubscribed: subscribedThemes.some((subscribedTheme) => subscribedTheme.id === theme.id)
+            }
+          );
+        });
+      });
     });
   }
 
+  public subscribeToTheme(themeId: string): void {
+    const payload = this.tokenService.getPayloadFromToken();
+
+    if (payload == null) {
+      return;
+    }
+
+    this.themeService.subscribe(Number(themeId)).subscribe(() => {
+      const theme = this.themes.find((t) => t.id === themeId);
+      if (theme) {
+        theme.isSubscribed = true;
+      }
+    });
+  }
+
+  public unsubscribeFromTheme(themeId: string): void {
+    const payload = this.tokenService.getPayloadFromToken();
+
+    if (payload == null) {
+      return;
+    }
+
+    this.themeService.unsubscribe(Number(themeId)).subscribe(() => {
+      const theme = this.themes.find((t) => t.id === themeId);
+      if (theme) {
+        theme.isSubscribed = false;
+      }
+    });
+  }
 }
