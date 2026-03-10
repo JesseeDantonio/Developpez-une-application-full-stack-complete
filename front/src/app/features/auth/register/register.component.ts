63eb0caa894c87cf19a9e7e94a527316428c3d04
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-10 13:13:07 +0100

    feat(register): ajout de la vérification des informations côté front

--- front/src/app/features/auth/register/register.component.ts
+++ front/src/app/features/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterRequest } from 'src/app/core/models/auth.model';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 
@@ -18,18 +18,53 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', {
+        validators: [Validators.required, this.passwordStrengthValidator()]
+      }]
     });
   }
 
   ngOnInit(): void {
   }
 
+  public passwordStrengthValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) return null; // On laisse le validateur 'required' gérer le champ vide
+
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+      const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value);
+      const isValidLength = value.length >= 8;
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecial && isValidLength;
+
+      if (!passwordValid) {
+        // Si invalide, on retourne un objet avec le détail de ce qui manque
+        return {
+          passwordStrength: {
+            hasUpperCase,
+            hasLowerCase,
+            hasNumeric,
+            hasSpecial,
+            isValidLength
+          }
+        };
+      }
+      return null; // Le mot de passe est valide
+    };
+  }
+
+  get passwordErrors() {
+    return this.registerForm.get('password')?.errors?.['passwordStrength'];
+  }
+
   onSubmit() {
-    console.log("Form submitted with values:", this.registerForm.value);
     if (this.registerForm.invalid) {
       return;
     }
+
     const credentials: RegisterRequest = {
       email: this.registerForm.value.email,
       password: this.registerForm.value.password,
@@ -38,7 +73,6 @@ export class RegisterComponent implements OnInit {
 
     this.authService.register(credentials).subscribe({
       next: (response) => {
-        console.log('Registration successful');
       },
       error: (err) => console.error('Erreur register', err)
     });
