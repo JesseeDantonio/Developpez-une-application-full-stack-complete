a4698a7d1b23e1e62dced95af651e02745d76113
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-11 11:21:56 +0100

    feat(profil-edit): intégration logique et affichage des themes abonné

--- front/src/app/features/profil/pages/profile-edit/profil-edit.component.ts
+++ front/src/app/features/profil/pages/profile-edit/profil-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Payload } from 'src/app/core/models/payload.interface';
+import { Theme } from 'src/app/core/models/theme.interface';
+import { TokenService } from 'src/app/core/services/token.service';
+import { ThemeService } from 'src/app/features/theme/services/theme.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,6 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './profil-edit.component.html',
   styleUrl: './profil-edit.component.scss',
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit {
+  public themes: Theme[] = [];
+
+  constructor(private themeService: ThemeService, private tokenService: TokenService) { }
+
+  ngOnInit(): void {
+    const payload: Payload | null = this.tokenService.getPayloadFromToken();
+
+    if (payload == null) {
+      return;
+    }
+
+    this.themeService.getAll().subscribe((themes) => {
+      this.themeService.getSubscribedThemes(payload.sub).subscribe((subscribedThemes) => {
+        themes.forEach((theme) => {
+          const subscribed = subscribedThemes.some((subscribedTheme) => subscribedTheme.id == theme.id);
+          if (subscribed) {
+            this.themes.push(
+              {
+                id: theme.id,
+                name: theme.name.toString(),
+                description: theme.description.toString(),
+                isSubscribed: subscribed
+              }
+            );
+          }
+        });
+      });
+    });
+  }
 
 }
