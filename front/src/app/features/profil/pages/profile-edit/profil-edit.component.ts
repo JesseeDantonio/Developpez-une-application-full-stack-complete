3de6c4f984878b2f09d31405228a465731d0cfa9
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-12 16:35:48 +0100

    feat(profil-edit): ajout de la gestion du formulaire réactif et de la mise à jour du profil utilisateur

--- front/src/app/features/profil/pages/profile-edit/profil-edit.component.ts
+++ front/src/app/features/profil/pages/profile-edit/profil-edit.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RegisterRequest } from 'src/app/core/models/auth.model';
 import { Payload } from 'src/app/core/models/payload.interface';
 import { Theme } from 'src/app/core/models/theme.interface';
 import { TokenService } from 'src/app/core/services/token.service';
 import { ThemeService } from 'src/app/features/theme/services/theme.service';
+import { UserService } from 'src/app/features/user/services/user.service';
 
 @Component({
   selector: 'app-profil',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profil-edit.component.html',
   styleUrl: './profil-edit.component.scss',
 })
 export class ProfilComponent implements OnInit {
   public themes: Theme[] = [];
+  public formGroup!: FormGroup;
 
-  constructor(private themeService: ThemeService, private tokenService: TokenService) { }
+  constructor(private themeService: ThemeService, private tokenService: TokenService, private fb: FormBuilder, private userService: UserService) {
+    this.formGroup = this.fb.group({
+      name: [''],
+      email: [''],
+      password: ['']
+    });
+  }
 
   ngOnInit(): void {
     const payload: Payload | null = this.tokenService.getPayloadFromToken();
@@ -41,4 +51,26 @@ export class ProfilComponent implements OnInit {
     });
   }
 
+  onSubmit(): void {
+    const payload: Payload | null = this.tokenService.getPayloadFromToken();
+
+    if (payload == null) {
+      return;
+    }
+
+    const updatedUserData = {
+      name: this.formGroup.get('name')?.value,
+      email: this.formGroup.get('email')?.value,
+      password: this.formGroup.get('password')?.value
+    };
+
+    this.userService.updateProfile(updatedUserData).subscribe({
+      next: (response) => {
+        alert('Profil mis à jour avec succès !');
+      },
+      error: (error) => {
+        alert('Erreur lors de la mise à jour du profil. Veuillez réessayer.');
+      }
+    });
+  }
 }
