62a8f0126f0fc3423c62a41fc9dd08c8b6776be7
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-15 11:32:40 +0100

    feat(dto): ajout de la classe MessageResponse pour encapsuler les messages de réponse

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/MessageResponse.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.mddapi.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+}
