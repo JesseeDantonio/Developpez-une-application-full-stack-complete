a174848fd627554d3a6f0637683743b2ef4b417f
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-15 17:36:17 +0100

    feat(user-controller): uniformisation des retours avec ResponseEntity pour standardiser les réponses API

--- back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -3,6 +3,8 @@ package com.openclassrooms.mddapi.controller;
 import com.openclassrooms.mddapi.dto.in.UserCreateDTO;
 import com.openclassrooms.mddapi.dto.out.UserDTO;
 import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +20,36 @@ public class UserController {
 
     // GET /api/users
     @GetMapping
-    public List<UserDTO> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 
     // GET /api/users/{id}
     @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable Integer id) {
-        return userService.getUserById(id);
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer id) {
+        UserDTO userDTO = userService.getUserById(id);
+        return ResponseEntity.ok(userDTO);
     }
 
     // POST /api/users
     @PostMapping
-    public UserCreateDTO createUser(@RequestBody UserCreateDTO userDto) {
-        return userService.createUser(userDto);
+    public ResponseEntity<UserCreateDTO> createUser(@RequestBody UserCreateDTO userDto) {
+        UserCreateDTO createdUser = userService.createUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     // PUT /api/users/{id}
     @PutMapping("/{id}")
-    public UserDTO updateUser(@PathVariable Integer id, @RequestBody UserCreateDTO userDto) {
-        return userService.updateUser(id, userDto);
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Integer id, @RequestBody UserCreateDTO userDto) {
+        UserDTO updatedUser = userService.updateUser(id, userDto);
+        return ResponseEntity.ok(updatedUser);
     }
 
     // DELETE /api/users/{id}
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
