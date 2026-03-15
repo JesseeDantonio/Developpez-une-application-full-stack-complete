fc319da6b84a754cd32a0db141b261f51c94166e
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-15 17:57:27 +0100

    feat(article-controller): uniformisation des retours avec ResponseEntity pour standardiser les réponses API

--- back/src/main/java/com/openclassrooms/mddapi/controller/ArticleController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/ArticleController.java
@@ -3,6 +3,8 @@ package com.openclassrooms.mddapi.controller;
 import com.openclassrooms.mddapi.dto.in.CreateArticleDTO;
 import com.openclassrooms.mddapi.dto.out.ArticleDTO;
 import com.openclassrooms.mddapi.service.ArticleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +20,36 @@ public class ArticleController {
 
     // GET /api/articles/{id}
     @GetMapping("/{id}")
-    public ArticleDTO getArticleById(@PathVariable Integer id) {
-        return articleService.getArticleById(id);
+    public ResponseEntity<ArticleDTO> getArticleById(@PathVariable Integer id) {
+        ArticleDTO articleDTO = articleService.getArticleById(id);
+        return ResponseEntity.ok(articleDTO);
     }
 
     // GET /api/articles
     @GetMapping
-    public List<ArticleDTO> getAllArticles() {
-        return articleService.getAllArticles();
+    public ResponseEntity<List<ArticleDTO>> getAllArticles() {
+        List<ArticleDTO> articles = articleService.getAllArticles();
+        return ResponseEntity.ok(articles);
     }
 
     // POST /api/articles
     @PostMapping
-    public CreateArticleDTO createArticle(@RequestBody CreateArticleDTO articleDto) {
-        return articleService.createArticle(articleDto);
+    public ResponseEntity<CreateArticleDTO> createArticle(@RequestBody CreateArticleDTO articleDto) {
+        CreateArticleDTO createdArticle = articleService.createArticle(articleDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdArticle);
     }
 
     // PUT /api/articles/{id}
     @PutMapping("/{id}")
-    public ArticleDTO updateArticle(@PathVariable Integer id, @RequestBody CreateArticleDTO articleDto) {
-        return articleService.updateArticle(id, articleDto);
+    public ResponseEntity<ArticleDTO> updateArticle(@PathVariable Integer id, @RequestBody CreateArticleDTO articleDto) {
+        ArticleDTO updatedArticle = articleService.updateArticle(id, articleDto);
+        return ResponseEntity.ok(updatedArticle);
     }
 
     // DELETE /api/articles/{id}
     @DeleteMapping("/{id}")
-    public void deleteArticle(@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteArticle(@PathVariable Integer id) {
         articleService.deleteArticle(id);
+        return ResponseEntity.noContent().build();
     }
 }
