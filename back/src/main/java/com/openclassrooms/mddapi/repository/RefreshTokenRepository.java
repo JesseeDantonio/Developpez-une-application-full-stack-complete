2e45594f83627fedfc43ea9df21f416176046f02
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-03-20 13:47:08 +0100

    refactor(auth): suppression des entités et du dépôt de `RefreshToken` inutilisés

--- back/src/main/java/com/openclassrooms/mddapi/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.openclassrooms.mddapi.repository;
-
-import com.openclassrooms.mddapi.entity.RefreshTokenEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Integer> {
-}
